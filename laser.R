998dd8acb596e7b6e23678f00f753d4850270ad0
Christopher Lorton <github@lortons.net>
2025-02-22 00:32:18 +0000

    cleanup based on lintr

--- laser.R
+++ laser.R
@@ -3,16 +3,16 @@
 install.packages("reticulate")
 
 library(reticulate)
-reticulate::virtualenv_create('r-reticulate', version='3.12')
-reticulate::virtualenv_install(envname='r-reticulate', packages='git+https://github.com/InstituteforDiseaseModeling/laser-measles.git@allhands')
+reticulate::virtualenv_create("r-reticulate", version="3.12")
+reticulate::virtualenv_install(envname="r-reticulate", packages="git+https://github.com/InstituteforDiseaseModeling/laser-measles.git@allhands")
 
 lm <- import("laser_measles")
 
-parameters <- lm$generic$get_parameters(py_dict(list('seed', 'pdf'), list(20250220, TRUE)))
+parameters <- lm$generic$get_parameters(py_dict(list("seed", "pdf"), list(20250220, TRUE)))
 scenario <- lm$generic$get_scenario(parameters, parameters$verbose)
 model <- lm$Model(scenario, parameters)
 
-model$components = list(
+model$components <- list(
   lm$Births,
   lm$NonDiseaseDeaths,
   lm$Susceptibility,
@@ -26,8 +26,8 @@ model$components = list(
 # seed_infections_randomly(model, ninfections=100)
 # Seed initial infections in Node 13 (King County) at the start of the simulation
 # Pierce County is Node 18, Snohomish County is Node 14, Yakima County is 19
-lm$utils$seed_infections_in_patch(model, ipatch=13, ninfections=100)
+lm$utils$seed_infections_in_patch(model, ipatch = 13, ninfections = 100)
 
 model$run()
 
-model$visualize(pdf=parameters$pdf)
+model$visualize(pdf = parameters$pdf)
